3b3fe7a11f1df0ba2ac64fac3efdb1b35014ed29
d4rk-lucif3r <lucifer78908@gmail.com>
2021-09-15 13:30:11 +0530

    Update 0.0.26 - Changed Name
    Signed-off-by: d4rk-lucif3r <lucifer78908@gmail.com>

--- Dhanrakshak.py
+++ Dhanrakshak.py
@@ -1,16 +1,19 @@
+#!python
+#cython: language_level=3
+
 import sys
 
 from PySide2.QtCore import QUrl
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtWidgets import QApplication
 
-from helpers.slots import ValUpdate
+from helpers.connection import Connection
 
 
 def runQML():
 
     app = QApplication(sys.argv)
-    slot = ValUpdate()
+    slot = Connection()
     engine = QQmlApplicationEngine()
     engine.rootContext().setContextProperty("slot", slot)
     engine.load(QUrl.fromLocalFile('screens/Segregation.qml'))
